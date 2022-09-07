a0939e23e2850776a984d7ef54f854f936c7c93c
KidNova <101372394+K1DN0V4@users.noreply.github.com>
2022-09-07 16:10:19 +0000

    Created method room1 and fixed previous methods

--- Main.java
+++ Main.java
@@ -1,5 +1,70 @@
+
+// imports
+import java.util.Scanner;
+import java.util.ArrayList;
+
+// main class
 class Main {
+
+  ArrayList<String> list = new ArrayList<String>();
+  //Scanner input = new Scanner(System.in);
+
+  static int easterEggs = 0;
+  static boolean flowerTaken = false;
+  static boolean techxTaken = false;
+  static boolean key = false;
+  static boolean taken = true;
+  static boolean keanuReevesSeen = false;
+  static String flowerName = "";
+  static double e19_urmom;
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    System.out.println(
+        "Hello world! \nAre you ready to adventure into the Deep Dark? \nThe commands you will be using thoughout this adventure are as follows: yes, no, interact, inventory, commands, attack, north, east, south, and west");
+    System.out.println(
+        "If you need to use 'yes', 'no', or if you need to enter a cutsom word, then you will be prompted.\nOtherwise, you can try any of the other commands. If you need to refer back to the commands then simply type in commands when you are making a decision.");
+    System.out.println(
+        "Interact will try to interact with anything in the room.\nAttack will attempt to kill the enemy.\nAny of the movement words with move you into the room in that direction.\nInventory will display the items you currently have.");
+    System.out.println("Lets begin, shall we? (yes/no)");
+    startRoom();
+
+  } // end main method
+
+  /* ----- Room methods ----- */
+  public static void startRoom() {
+    if (prompt().equals("yes")) {
+      System.out.println("Wondeful! I hope you enjoy your adventure, traveler.");
+    } else {
+      System.out.println("That's too bad, play now");
+    }
+    room1();
+  } // end startRoom
+
+  public static void room1(){
+    System.out.println("You fall down a hole and miraculously don't die. Nice!! \nAll you can see is a single yellow dandelion. \nWhat do you want to name it? (Type in a response)");
+    flowerName = prompt();
+    if (flowerName.equals("flowey")) {
+      System.out.println("You named your flower " + flowerName + ". Nice one, very based.");
+      easterEggs++;
+    }
+    else if (flowerName.equals("Marcus")) {
+      System.out.println("You named your flower " + flowerName + ". ur mom lol gottem.");
+      easterEggs++;
+    }
+    else {
+      System.out.println("You named your flower " + flowerName + ".");
+    }
   }
-}
+
+  /* ----- Non room methods ----- */
+
+  public static String prompt() {
+    Scanner input = new Scanner(System.in);
+    String text = input.nextLine();
+    input.close();
+    return text;
+  } // end prompt method
+
+} // end main class
---
+++ README.md
@@ -0,0 +1,6 @@
+## Hello World!
+
+This is game that some dumb people made because why not.
+It should not have taken as long as it did but as I mentioned before we are dumb.
+
+Por- I mean Github link: https://github.com/K1DN0V4/DentalPureAfkGamingTEST2
